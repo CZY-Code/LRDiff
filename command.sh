fe09c9a56f365377f28a4aac52d5f506ae0427fc
chengzy <46067459+CZY-Code@users.noreply.github.com>
2025-10-29 22:17:25 +0800

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,10 +1,2 @@
-#train structure denoising
-python train/structure/config/inpainting/train.py
-# train texture denoising
-python train/texture/config/inpainting/train.py
-# train discriminator
-python train/discriminator/config/inpainting/train.py
-
-#train tdm
 python tdm/train.py
 python tdm/test.py
